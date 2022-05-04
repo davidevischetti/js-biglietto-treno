67bd00d4e1b27d6b55a2524a456dc2df7ad39e30
Davide <davidevischetti@gmail.com>
2022-05-04 18:14:13 +0200

    esercizio corretto

--- script.js
+++ script.js
@@ -2,28 +2,27 @@
 let price;
 let ticketPrice;
 
-
 // Ask the user his own age
 const age = prompt('How old are you?');
-
+// debug
 console.log(age);
 
 // Ask the user how many km his travel is
 const km = prompt('How many km do you have to travel?');
-
+// debug
 console.log(km);
 
-// Calculate ticket price
+// Calculate price
 
-// Calculate ticket price for Under18 users
+// Calculate price for Under18 users
 if (age < 18) {
     price = ((0.21 - (0.21 / 100 * 20)) * km);
 } 
-// Calculate ticket price for Over65 users
+// Calculate price for Over65 users
 else if (age >= 65) {
     price = ((0.21 - (0.21 / 100 * 40)) * km);
 }
-// Calculate ticket price for standard users
+// Calculate price for standard users
 else {
     price = 0.21 * km;
 }
@@ -31,20 +30,7 @@ else {
 // Calculate ticket price for humans that can't easily understend too much long numbers
 ticketPrice = (Math.round(price * 100) / 100).toFixed(2);
 
-if (age < 18) {
-    // Under18 ticket price
-    document.getElementById('ticket-under-18').innerHTML = `Your ticket price is ${ticketPrice} Euro`
-} 
-// Calculate ticket price for Over65 users
-else if (age >= 65) {
-    // Over65 ticket price
-    document.getElementById('ticket-over-65').innerHTML = `Your ticket price is ${ticketPrice} Euro`
-}
-// Calculate ticket price for standard users
-else {
-    // Standard ticket price
-    document.getElementById('ticket-standard').innerHTML = `Your ticket price is ${ticketPrice} Euro`
-}
+document.getElementById('ticket').innerHTML = `Your ticket price is ${ticketPrice} Euro`
 
 // debug
 console.log(ticketPrice);
